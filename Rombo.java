eee633e58f26ccc6d3d9ba732b43e748edca88b8
sromeropo <160175381+sromeropo@users.noreply.github.com>
2024-03-14 21:29:11 -0500

    Create Rombo.java

---
+++ Rombo.java
@@ -0,0 +1,19 @@
+package act2parte2;
+
+public class Rombo {
+    double x; // Diagonal mayor del rombo
+    double y; // Diagonal menor del rombo
+
+    Rombo(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double calcularArea() {
+        return x*y/2;
+    }
+
+    double calcularPerímetro() {
+        return 4*(Math.pow((x*x)+(y*y),0.5)/2);
+    }
+}
